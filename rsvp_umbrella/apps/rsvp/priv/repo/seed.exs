62cf9c7a4d70eab46453f781cf86d1e49c7505c5
Karan Sharma <cserkaran@gmail.com>
2017-12-11 23:44:16 +1100

    Seeding database

---
+++ rsvp_umbrella/apps/rsvp/priv/repo/seed.exs
@@ -0,0 +1,2 @@
+Rsvp.EventQueries.create(Rsvp.Events.changeset(%Rsvp.Events{}, %{date: "2018-05-23 09:00:00", title: "Summer Codefest", location: "Omaha"}))
+Rsvp.EventQueries.create(Rsvp.Events.changeset(%Rsvp.Events{}, %{date: "2018-06-19 09:00:00", title: "Hackathon Junio", location: "London"}))
